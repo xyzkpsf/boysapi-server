c3da0659d0e1786f1f5f8dc20b20177017cd0dd0
xyzkpsf <xyzkpsf@gmail.com>
2022-12-24 19:41:47 -0800

    fix port

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 80;
+const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 export const config = {
     mongo: {
