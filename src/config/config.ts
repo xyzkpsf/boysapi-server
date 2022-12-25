69c5b597d57c2190516b5872a9889eb6344884c5
xyzkpsf <xyzkpsf@gmail.com>
2022-12-24 18:11:06 -0800

    change to heroku env config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,15 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGO_USER = process.env.MONGO_USER || '';
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.u3bsi.mongodb.net/test`;
-
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
 
 export const config = {
     mongo: {
-        url: MONGO_URL
+        url: process.env.MONGODB_URI
     },
     server: {
         port: SERVER_PORT
